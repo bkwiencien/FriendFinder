6eb0ba46c33a63b999742d24cd2b02810e556a3f
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-10 13:54:40 -0400

    inline script

--- app/public/script.js
+++ app/public/script.js
@@ -1,7 +1,9 @@
-$("#button1").click(function() {
+$('#button1').on("click",(function(event) {
+	  console.log("in click fnct");
+	  event.preventDefault();
       console.log("handle submit");
       alert("button clicked");
-      });
+      }));
 function myFunction() { 
  alert("clicked");
  console.log("in myFunction");
