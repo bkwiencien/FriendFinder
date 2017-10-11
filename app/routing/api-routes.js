05c0d4db94b32f5eccf2c8d7e425c6604f31fef7
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-11 04:30:04 -0400

    scores

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -7,12 +7,19 @@ module.exports = function(app) {
 
     app.post("/api/data",function(req,res){
     	console.log("in post");
+        var userScores = [];
+        var userTotal  = 0;
     	var best = {
     		name: "test-name",
     		photo: "test-photo",
     		diff:  100
     	};
+        userScores = req.param("scores");
+        for (j=0;j<10;j++) {
+            userTotal = userTotal + parseFloat(userScores[j]);
+        }
         res.json(best);
+        console.log("userTotal was " + userTotal);
         console.log("i was sent " + req.param("name"));
         console.log("i was also sent " + req.param("scores"));
     });
