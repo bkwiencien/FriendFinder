b65785b7168a8c75ab9182a8bec7d4878f0c6f61
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-11 04:43:41 -0400

    accumulate differences

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -9,15 +9,29 @@ module.exports = function(app) {
     	console.log("in post");
         var userScores = [];
         var userTotal  = 0;
+        var userName = "";
+        var userPhoto = "";
+        var diff = 0;
     	var best = {
     		name: "test-name",
     		photo: "test-photo",
     		diff:  100
     	};
         userScores = req.param("scores");
-        for (j=0;j<10;j++) {
-            userTotal = userTotal + parseFloat(userScores[j]);
-        }
+        userName=req.param("name");
+        userPhoto=req.param("photo");
+        for (i=0;i<friends.length;i++) {
+          diff = 0;
+          for (j=0;j<10;j++) {
+              var s = parseFloat(userScores[j]);
+              diff = diff + Math.abs(s-friends[i].scores[j]);
+          }
+          if (diff < best.diff) {
+            best.name = friends[i].name;
+            best.photo = friends[i].photo;
+            best.diff = diff;
+          }  
+         }
         res.json(best);
         console.log("userTotal was " + userTotal);
         console.log("i was sent " + req.param("name"));
